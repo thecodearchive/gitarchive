cc6775fb1db5c453e4a1ed2839243168d112217e
Filippo Valsorda <filippo@cloudflare.com>
2016-03-29 03:26:27 +0200

    drink: refactor into a Drinker object

---
+++ cmd/drink/drinker.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"expvar"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"sync/atomic"
+	"time"
+
+	"github.com/thecodearchive/gitarchive/camli"
+	"github.com/thecodearchive/gitarchive/github"
+	"github.com/thecodearchive/gitarchive/queue"
+)
+
+type Drinker struct {
+	q  *queue.Queue
+	st *github.StarTracker
+	u  *camli.Uploader
+
+	exp       *expvar.Map
+	expEvents *expvar.Map
+	expLatest *expvar.String
+
+	closing uint32
+}
+
+func (d *Drinker) DrinkArchive(a io.Reader) error {
+	r, err := github.NewTimelineArchiveReader(a)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	for atomic.LoadUint32(&d.closing) == 0 {
+		var e github.Event
+		if err := r.Read(&e); err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		d.expEvents.Add(e.Type, 1)
+		d.expLatest.Set(e.CreatedAt.String())
+
+		switch e.Type {
+		case "PushEvent":
+			d.handlePushEvent(&e)
+
+		case "CreateEvent":
+			var ce github.CreateEvent
+			if err := json.Unmarshal(e.Payload, &ce); err != nil {
+				d.exp.Add("dropped", 1)
+				log.Printf("[-] CreateEvent Unmarshal error: %s; dropped event: %#v", err, e)
+				continue
+			}
+			switch ce.RefType {
+			case "repository":
+				d.st.CreateEvent(e.Repo.Name, "", e.CreatedAt.Time)
+			case "branch", "tag":
+				// TODO
+			}
+
+		case "WatchEvent":
+			d.st.WatchEvent(e.Repo.Name, e.CreatedAt.Time)
+
+		case "ForkEvent":
+			var fe github.ForkEvent
+			if err := json.Unmarshal(e.Payload, &fe); err != nil {
+				d.exp.Add("dropped", 1)
+				log.Printf("[-] ForkEvent Unmarshal error: %s; dropped event: %#v", err, e)
+				continue
+			}
+			d.st.CreateEvent(fe.Forkee.FullName, e.Repo.Name, e.CreatedAt.Time)
+
+		case "DeleteEvent":
+			// TODO
+
+		case "PublicEvent":
+			// TODO
+		}
+	}
+
+	if atomic.LoadUint32(&d.closing) == 1 {
+		return StoppedError
+	}
+
+	return nil
+}
+
+func (d *Drinker) handlePushEvent(e *github.Event) {
+	url := "https://github.com/" + e.Repo.Name + ".git"
+	repo, err := d.u.GetRepo(url)
+	if err != nil {
+		d.exp.Add("dropped", 1)
+		log.Printf("[-] Camli error: %s; dropped event: %#v", err, e)
+		return
+	}
+	if repo != nil {
+		d.exp.Add("gotrepo", 1)
+		d.exp.Add("queued", 1)
+		d.q.Add(e.Repo.Name, repo.Parent)
+	}
+
+	stars, parent, err := d.st.Get(e.Repo.Name)
+	if rate := github.IsRateLimit(err); rate != nil {
+		d.exp.Add("ratehits", 1)
+		log.Println("[-] Hit GitHub ratelimits, sleeping until", rate.Reset)
+		if interruptableSleep(rate.Reset.Sub(time.Now())) {
+			log.Println("[+] Resuming...")
+			d.handlePushEvent(e)
+		}
+		return
+	}
+	if github.Is404(err) {
+		d.exp.Add("vanished", 1)
+		return
+	}
+	if err != nil {
+		d.exp.Add("dropped", 1)
+		log.Printf("[-] ST error: %s; dropped event: %#v", err, e)
+		return
+	}
+	if stars < 10 {
+		d.exp.Add("skipped", 1)
+		return
+	}
+
+	d.exp.Add("queued", 1)
+	d.q.Add(e.Repo.Name, parent)
+}
+
+var StoppedError = errors.New("process was asked to gracefully stop")
+
+func (d *Drinker) Stop() {
+	atomic.StoreUint32(&d.closing, 1)
+}
+
+func interruptableSleep(d time.Duration) bool {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	defer signal.Stop(c)
+	t := time.NewTimer(d)
+	select {
+	case <-c:
+		return false
+	case <-t.C:
+		return true
+	}
+}
--- cmd/drink/main.go
+++ cmd/drink/main.go
@@ -1,34 +1,20 @@
 package main
 
 import (
-	"encoding/json"
 	"expvar"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
 	"os/signal"
-	"sync/atomic"
 	"time"
 
 	"github.com/thecodearchive/gitarchive/camli"
 	"github.com/thecodearchive/gitarchive/github"
 	"github.com/thecodearchive/gitarchive/queue"
 )
-
-var (
-	exp       = expvar.NewMap("drink")
-	expEvents = new(expvar.Map).Init()
-	expLatest = new(expvar.String)
-)
-
-func init() {
-	exp.Set("latestevent", expLatest)
-	exp.Set("events", expEvents)
-}
 
 func main() {
 	go func() {
@@ -43,13 +29,10 @@ func main() {
 		log.Fatal("usage: drink 2016-01-02-15")
 	}
 
-	uploader := camli.NewUploader()
-
 	if os.Getenv("GITHUB_TOKEN") == "" {
 		log.Fatal("Please set the env var GITHUB_TOKEN")
 	}
 	st := github.NewStarTracker(1000000000, os.Getenv("GITHUB_TOKEN"))
-	exp.Set("github", st.Expvar())
 
 	if f, err := os.Open(*cachePath); err != nil {
 		log.Println("[ ] Can't load StarTracker cache, starting empty")
@@ -58,15 +41,6 @@ func main() {
 		st.LoadCache(f)
 		f.Close()
 	}
-
-	var closing uint32
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	go func() {
-		<-c
-		log.Println("[ ] Terminating gracefully...")
-		atomic.StoreUint32(&closing, 1)
-	}()
 
 	log.Println("[ ] Opening queue...")
 	q, err := queue.Open(*queuePath)
@@ -83,117 +57,42 @@ func main() {
 		fatalIfErr(f.Close())
 	}()
 
-	log.Println("[ ] Opening archive download...")
+	exp := expvar.NewMap("drink")
+	expEvents := new(expvar.Map).Init()
+	expLatest := new(expvar.String)
+	exp.Set("latestevent", expLatest)
+	exp.Set("events", expEvents)
+	exp.Set("github", st.Expvar())
+
+	d := &Drinker{
+		q: q, st: st, u: camli.NewUploader(),
+		exp: exp, expEvents: expEvents, expLatest: expLatest,
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		log.Println("[ ] Stopping gracefully...")
+		d.Stop()
+	}()
+
 	t, err := time.Parse(github.HourFormat, flag.Arg(0))
 	fatalIfErr(err)
+
+	log.Println("[ ] Opening archive download...")
 	a, err := github.DownloadArchive(t)
 	fatalIfErr(err)
 	defer a.Close()
-	r, err := github.NewTimelineArchiveReader(a)
-	fatalIfErr(err)
-	defer r.Close()
-
-	log.Println("[ ] Reading events...")
-	for atomic.LoadUint32(&closing) == 0 {
-		var e github.Event
-		if err := r.Read(&e); err == io.EOF {
-			break
-		} else {
-			fatalIfErr(err)
-		}
-
-		expEvents.Add(e.Type, 1)
-		expLatest.Set(e.CreatedAt.String())
-
-		switch e.Type {
-		case "PushEvent":
-			url := "https://github.com/" + e.Repo.Name + ".git"
-			repo, err := uploader.GetRepo(url)
-			if err != nil {
-				exp.Add("dropped", 1)
-				log.Printf("[-] Camli error: %s; dropped event: %#v", err, e)
-				continue
-			}
-			if repo != nil {
-				exp.Add("gotrepo", 1)
-				exp.Add("queued", 1)
-				q.Add(e.Repo.Name, repo.Parent)
-			}
-
-			stars, parent, err := st.Get(e.Repo.Name)
-			if rate := github.IsRateLimit(err); rate != nil {
-				exp.Add("ratehits", 1)
-				log.Println("[-] Hit GitHub ratelimits, sleeping until", rate.Reset)
-				interruptableSleep(rate.Reset.Sub(time.Now()))
-
-				// TODO: retry, both here and for the other "dropped" cases
-				exp.Add("dropped", 1)
-				log.Printf("[-] Resuming; dropped event: %#v", e)
-				continue
-			}
-			if github.Is404(err) {
-				exp.Add("vanished", 1)
-				continue
-			}
-			if err != nil {
-				exp.Add("dropped", 1)
-				log.Printf("[-] ST error: %s; dropped event: %#v", err, e)
-				continue
-			}
-			if stars < 10 {
-				exp.Add("skipped", 1)
-				continue
-			}
-
-			exp.Add("queued", 1)
-			q.Add(e.Repo.Name, parent)
-
-		case "CreateEvent":
-			var ce github.CreateEvent
-			fatalIfErr(json.Unmarshal(e.Payload, &ce))
-			switch ce.RefType {
-			case "repository":
-				st.CreateEvent(e.Repo.Name, "", e.CreatedAt.Time)
-			case "branch", "tag":
-				// TODO
-			}
-
-		case "WatchEvent":
-			st.WatchEvent(e.Repo.Name, e.CreatedAt.Time)
-
-		case "ForkEvent":
-			var fe github.ForkEvent
-			fatalIfErr(json.Unmarshal(e.Payload, &fe))
-			st.CreateEvent(fe.Forkee.FullName, e.Repo.Name, e.CreatedAt.Time)
-
-		case "DeleteEvent":
-			// TODO
-
-		case "PublicEvent":
-			// TODO
-		}
-	}
+	log.Println("[ ] Consuming...")
+	fatalIfErr(d.DrinkArchive(a))
 
 	log.Println("[+] Processed events until", expLatest)
-
 	fmt.Print(exp.String())
 }
 
 func fatalIfErr(err error) {
 	if err != nil {
 		log.Panic(err) // panic to let the defer run
-	}
-}
-
-func interruptableSleep(d time.Duration) bool {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	defer signal.Stop(c)
-	t := time.NewTimer(d)
-	select {
-	case <-c:
-		return false
-	case <-t.C:
-		return true
 	}
 }
